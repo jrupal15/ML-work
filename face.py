7522fb48531d1db1a3f7a81643f6c7b87ad416db
jrupal15 <39940646+jrupal15@users.noreply.github.com>
2018-06-26 12:56:21 +0530

    Add files via upload
    face detection using python

---
+++ face.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import cv2
+import random
+face_data=cv2.CascadeClassifier('har.xml')
+
+cap=cv2.VideoCapture(0)
+
+while cap.isOpened() :
+	status,frame=cap.read()
+	grayimg=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	faces=face_data.detectMultiScale(grayimg,1.15,5)
+	for(x,y,w,h) in faces :
+		cv2.rectangle(frame,(x,y),(x+w,y+h), (0,225,0),2)
+		roi_gray=grayimg[y:y+h,x:x+w]
+		roi_coloor=frame[y:y+h,x:x+w]
+#showing current image
+
+	cv2.imshow("current",frame)
+#to get random image
+	x=random.random()
+	y=str(x)[5:8]
+	
+	cv2.imwrite('adh'+y+'.jpg',frame)
+	if cv2.waitKey(30) & 0xff==ord('w') :
+		break
+
+#destroying window
+cv2.DestroyAllWindows()
+cap.release()
+
+
